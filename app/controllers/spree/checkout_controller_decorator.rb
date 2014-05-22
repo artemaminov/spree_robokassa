13549ad7bdef8b7bb3147faf11bc23ddd2f9e6c5
Artem Aminov <artemaminov@gmail.com>
2014-05-22 23:41:44 +0600

    Move helper include to Engine

--- app/controllers/spree/checkout_controller_decorator.rb
+++
@@ -1,6 +0,0 @@
-Spree::CheckoutController.class_eval do
- 
-  ActionView::Base.send(:include, ActiveMerchant::Billing::Integrations::ActionViewHelper)
- 
- 
-end
--- lib/spree_robokassa/engine.rb
+++ lib/spree_robokassa/engine.rb
@@ -2,16 +2,18 @@ module SpreeRobokassa
   class Engine < Rails::Engine
 
     config.autoload_paths += %W(#{config.root}/lib)
+    config.after_initialize do |app|
+      app.config.spree.payment_methods += [Spree::BillingIntegration::Robokassa]
+    end
+
+    initializer 'spree_robokassa' do
+      ActionView::Base.send :include, ActiveMerchant::Billing::Integrations::ActionViewHelper
+    end
 
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/spree/*_decorator*.rb")) do |c|
         Rails.env.production? ? require(c) : load(c)
       end
-
-      config.after_initialize do |app|
-        app.config.spree.payment_methods += [Spree::BillingIntegration::Robokassa]
-      end
-
     end
 
     config.to_prepare &method(:activate).to_proc
